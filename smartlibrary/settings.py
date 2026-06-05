9422caed36c83fa8fd0548fc4cd6535330d26e6c
Vinoth Kumar <vinothkumarj651@gmail.com>
2026-06-05 23:22:04 +0530

    Fixed email settings

--- smartlibrary/settings.py
+++ smartlibrary/settings.py
@@ -158,15 +158,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 EMAIL_HOST = os.getenv("EMAIL_HOST")
-
 EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
 
-EMAIL_USE_TLS = True
-
-EMAIL_USE_SSL = False
-
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+
+DEFAULT_FROM_EMAIL = os.getenv(
+    "DEFAULT_FROM_EMAIL",
+    "aurcm.smartlibrary@gmail.com"
+)
