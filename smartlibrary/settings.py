d2377d30825f9b005aed378b631d3cbeaac13397
Vinoth Kumar <vinothkumarj651@gmail.com>
2026-06-06 11:49:17 +0530

    Email config update

--- smartlibrary/settings.py
+++ smartlibrary/settings.py
@@ -157,15 +157,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
-EMAIL_HOST = os.getenv("EMAIL_HOST")
-EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
+EMAIL_HOST = "smtp-relay.brevo.com"
+EMAIL_PORT = 587
 
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_USER = "adadde001@smtp-brevo.com"
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
 EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
 
-EMAIL_TIMEOUT = 30
-
-DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
+DEFAULT_FROM_EMAIL = "aurcm.smartlibrary@gmail.com"
