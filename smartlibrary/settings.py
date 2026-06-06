9c144006741f25a9728070bc3bd05204f5c50f28
Vinoth Kumar <vinothkumarj651@gmail.com>
2026-06-06 10:53:36 +0530

    Fixed Brevo SMTP login

--- smartlibrary/settings.py
+++ smartlibrary/settings.py
@@ -164,9 +164,5 @@ EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
 EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
 
-DEFAULT_FROM_EMAIL = os.getenv(
-    "DEFAULT_FROM_EMAIL",
-    "aurcm.smartlibrary@gmail.com"
-)
+DEFAULT_FROM_EMAIL = "aurcm.smartlibrary@gmail.com"
